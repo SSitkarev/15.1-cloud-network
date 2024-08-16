b0bd7d9b73861ac3e4674289f4b04285055ee51e
Sergey <sergey.sitkarev@gmail.com>
2024-08-16 13:01:36 +0300

    1

---
+++ src/network.tf
@@ -0,0 +1,27 @@
+resource "yandex_vpc_network" "VPC" {
+  name = "VPC"
+}
+
+resource "yandex_vpc_subnet" "subnet-public" {
+  name           = "public"
+  zone           = var.a-zone
+  network_id     = yandex_vpc_network.VPC.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+resource "yandex_vpc_subnet" "subnet-private" {
+  name           = "private"
+  zone           = var.a-zone
+  network_id     = yandex_vpc_network.VPC.id
+  route_table_id = yandex_vpc_route_table.nat-route-table.id
+  v4_cidr_blocks = ["192.168.20.0/24"]
+}
+
+resource "yandex_vpc_route_table" "nat-route-table" {
+  network_id = yandex_vpc_network.VPC.id
+
+  static_route {
+    destination_prefix = "0.0.0.0/0"
+    next_hop_address   = var.nat-instance-ip
+  }
+}
